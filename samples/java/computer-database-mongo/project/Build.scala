515dba94ae88882516090d66af7a4abacfd429a6
Bjarte Stien Karlsen <bjarte@bjartek.org>
2012-08-17 22:29:12 +0200

    adding depenencies

--- samples/java/computer-database-mongo/project/Build.scala
+++ samples/java/computer-database-mongo/project/Build.scala
@@ -4,16 +4,20 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "computer-database"
+    val appName         = "computer-database-mongo"
     val appVersion      = "1.0"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+          "leodagdag"                         % "play2-morphia-plugin_2.9.1"  % "0.0.6"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+       resolvers ++= Seq(DefaultMavenRepository, Resolvers.githubRepository, Resolvers.morphiaRepository)
     )
 
+    object Resolvers {
+      val githubRepository = "LeoDagDag repository" at "http://leodagdag.github.com/repository/"
+      val morphiaRepository = "Morphia repository" at "http://morphia.googlecode.com/svn/mavenrepo/"
+    }
 }
             
